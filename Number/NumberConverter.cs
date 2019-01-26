0e0c8b56cb4cd4be947dd700d80c552c4fa7d6f6
liveinmegacity <33742639+liveinmegacity@users.noreply.github.com>
2019-01-26 20:19:46 +0700

    Add unit tests Add readme

---
+++ Number/NumberConverter.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Number
+{
+    static class NumberConverter
+    {
+        public enum CheckResult
+        {
+            OK,
+            Error
+        }
+
+        public struct Number
+        {
+            public string Roman { get; set; }
+            public int Arabic { get; set; }
+            public int Hierarchy { get; set; }
+        }
+
+        public static readonly int MaxValue = 3000;
+
+        public static readonly List<Number> Numbers = new List<Number>() {
+            new Number() { Roman =  "I", Arabic =    1, Hierarchy = 1 },
+            new Number() { Roman = "IV", Arabic =    4, Hierarchy = 2 },
+            new Number() { Roman =  "V", Arabic =    5, Hierarchy = 2 },
+            new Number() { Roman = "IX", Arabic =    9, Hierarchy = 2 },
+            new Number() { Roman =  "X", Arabic =   10, Hierarchy = 2 },
+            new Number() { Roman = "XL", Arabic =   40, Hierarchy = 3 },
+            new Number() { Roman =  "L", Arabic =   50, Hierarchy = 3 },
+            new Number() { Roman = "XC", Arabic =   90, Hierarchy = 3 },
+            new Number() { Roman =  "C", Arabic =  100, Hierarchy = 3 },
+            new Number() { Roman = "CD", Arabic =  400, Hierarchy = 4 },
+            new Number() { Roman =  "D", Arabic =  500, Hierarchy = 4 },
+            new Number() { Roman = "CM", Arabic =  900, Hierarchy = 4 },
+            new Number() { Roman =  "M", Arabic = 1000, Hierarchy = 4 }
+        };
+
+        public static CheckResult CheckArabic(int value)
+        {
+            if (value <= 0 || value > MaxValue) return CheckResult.Error;
+            return CheckResult.OK;
+        }
+
+        public static CheckResult CheckRoman(string value)
+        {
+            value = value.ToUpper();
+
+            if (string.IsNullOrEmpty(value)) return CheckResult.Error;
+
+            Match m = Regex.Match(value, "[^IVXLCDM]+", RegexOptions.IgnoreCase);
+            if (m.Success) return CheckResult.Error;
+
+            m = Regex.Match(value, "(I{4,}|X{4,}|C{4,}|M{4,})", RegexOptions.IgnoreCase);
+            if (m.Success) return CheckResult.Error;
+
+            int i = 0;
+            while ((i + 1) < value.Length)
+            {
+                Number left = Numbers.Find(RomanNumber => RomanNumber.Roman == value[i].ToString());
+                Number right = Numbers.Find(RomanNumber => RomanNumber.Roman == value[i + 1].ToString());
+
+                if ((right.Hierarchy - left.Hierarchy) > 1) return CheckResult.Error;
+                i++;
+            }
+
+            return CheckResult.OK;
+        }
+
+        public static string ToRoman(int value)
+        {
+            string outValue = "";
+            for (int i = Numbers.Count - 1; i >= 0; i--)
+            {
+                outValue += string.Concat(Enumerable.Repeat(Numbers[i].Roman, value / Numbers[i].Arabic));
+                value %= Numbers[i].Arabic;
+            }
+            return outValue;
+        }
+
+        public static int ToArabic(string value)
+        {
+            value = value.ToUpper();
+            int outValue = 0;
+            for (int i = 0; i < value.Length - 1; i++)
+            {
+                int left = Numbers.Find(RomanNumber => RomanNumber.Roman == value[i].ToString()).Arabic;
+                int right = Numbers.Find(RomanNumber => RomanNumber.Roman == value[i + 1].ToString()).Arabic;
+                outValue += (left < right) ? -left : left;
+            }
+            outValue += Numbers.Find(RomanNumber => RomanNumber.Roman == value[value.Length - 1].ToString()).Arabic;
+            return outValue;
+        }
+    }
+}
--- Number/Program.cs
+++ Number/Program.cs
@@ -1,115 +1,81 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text.RegularExpressions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Number
 {
-    static class NumberConverter
+    class Program
     {
-        public enum TCheckResult
-        {
-            OK,
-            Error
-        }
-
-        public struct TNumber
+        private struct ConvertCase
         {
             public string Roman { get; set; }
             public int Arabic { get; set; }
-            public int Hierarchy { get; set; }
+            public NumberConverter.CheckResult CheckResult { get; set; }
         }
 
-        public static readonly int MaxValue = 3000;
-
-        public static readonly List<TNumber> Numbers = new List<TNumber>() {
-            new TNumber() { Roman =  "I", Arabic =    1, Hierarchy = 1 },
-            new TNumber() { Roman = "IV", Arabic =    4, Hierarchy = 2 },
-            new TNumber() { Roman =  "V", Arabic =    5, Hierarchy = 2 },
-            new TNumber() { Roman = "IX", Arabic =    9, Hierarchy = 2 },
-            new TNumber() { Roman =  "X", Arabic =   10, Hierarchy = 2 },
-            new TNumber() { Roman = "XL", Arabic =   40, Hierarchy = 3 },
-            new TNumber() { Roman =  "L", Arabic =   50, Hierarchy = 3 },
-            new TNumber() { Roman = "XC", Arabic =   90, Hierarchy = 3 },
-            new TNumber() { Roman =  "C", Arabic =  100, Hierarchy = 3 },
-            new TNumber() { Roman = "CD", Arabic =  400, Hierarchy = 4 },
-            new TNumber() { Roman =  "D", Arabic =  500, Hierarchy = 4 },
-            new TNumber() { Roman = "CM", Arabic =  900, Hierarchy = 4 },
-            new TNumber() { Roman =  "M", Arabic = 1000, Hierarchy = 4 }
+        private static readonly List<ConvertCase> ConvertCases = new List<ConvertCase>()
+        {
+            new ConvertCase(){ Roman =              "I", Arabic =    1 /*, CheckResult = NumberConverter.CheckResult.OK*/ },
+            new ConvertCase(){ Roman =             "II", Arabic =    2 /*, CheckResult = NumberConverter.CheckResult.OK*/ },
+            new ConvertCase(){ Roman =             "xV", Arabic =   15 /*, CheckResult = NumberConverter.CheckResult.OK*/ },
+            new ConvertCase(){ Roman =            "XiX", Arabic =   19 /*, CheckResult = NumberConverter.CheckResult.OK*/ },
+            new ConvertCase(){ Roman =          "CDLXV", Arabic =  465 /*, CheckResult = NumberConverter.CheckResult.OK*/ },
+            new ConvertCase(){ Roman =          "mmCcC", Arabic = 2300 /*, CheckResult = NumberConverter.CheckResult.OK*/ },
+            new ConvertCase(){ Roman =  "MDCCCLXXXVIII", Arabic = 1888 /*, CheckResult = NumberConverter.CheckResult.OK*/ },
+            new ConvertCase(){ Roman = "MMDCCCLXXXVIII", Arabic = 2888 /*, CheckResult = NumberConverter.CheckResult.OK*/ },
+            new ConvertCase(){ Roman =       "MMCMXCIX", Arabic = 2999 /*, CheckResult = NumberConverter.CheckResult.OK*/ },
+            new ConvertCase(){ Roman =            "MMM", Arabic = 3000 /*, CheckResult = NumberConverter.CheckResult.OK*/ }
         };
 
-        public static TCheckResult CheckArabic(int value)
-        {
-            if (value <= 0 || value > MaxValue) return TCheckResult.Error;
-            return TCheckResult.OK;
-        }
-
-        public static TCheckResult CheckRoman(string value)
-        {
-            value = value.ToUpper();
-
-            if (string.IsNullOrEmpty(value)) return TCheckResult.Error;
-
-            Match m = Regex.Match(value, "[^IVXLCDM]+", RegexOptions.IgnoreCase);
-            if (m.Success) return TCheckResult.Error;
-
-            m = Regex.Match(value, "(I{4,}|X{4,}|C{4,}|M{4,})", RegexOptions.IgnoreCase);
-            if (m.Success) return TCheckResult.Error;
-
-            int i = 0;
-            while ((i + 1) < value.Length)
-            {
-                TNumber left = Numbers.Find(RomanNumber => RomanNumber.Roman == value[i].ToString());
-                TNumber right = Numbers.Find(RomanNumber => RomanNumber.Roman == value[i + 1].ToString());
-
-                if ((right.Hierarchy - left.Hierarchy) > 1) return TCheckResult.Error;
-                i++;
-            }
-
-            return TCheckResult.OK;
-        }
-
-        public static string ToRoman(int value)
-        {
-            string outValue = "";
-            for (int i = Numbers.Count - 1; i >= 0; i--)
-            {
-                outValue += string.Concat(Enumerable.Repeat(Numbers[i].Roman, value / Numbers[i].Arabic));
-                value %= Numbers[i].Arabic;
-            }
-            return outValue;
-        }
-
-        public static int ToArabic(string value)
-        {
-            value = value.ToUpper();
-            int outValue = 0;
-            for (int i = 0; i < value.Length - 1; i++)
-            {
-                int left = Numbers.Find(RomanNumber => RomanNumber.Roman == value[i].ToString()).Arabic;
-                int right = Numbers.Find(RomanNumber => RomanNumber.Roman == value[i + 1].ToString()).Arabic;
-                outValue += (left < right) ? -left : left;
-            }
-            outValue += Numbers.Find(RomanNumber => RomanNumber.Roman == value[value.Length - 1].ToString()).Arabic;
-            return outValue;
-        }
-    }
-
-    class Program
-    {
         static void Main(string[] args)
         {
-            //TNumber.CheckRoman("Hello, World!");
-            NumberConverter.CheckRoman("XiX");
-            Console.WriteLine(NumberConverter.ToArabic("XiX"));
-            //Console.WriteLine(TNumber.RomanToInt("Mim"));
-            Console.WriteLine(NumberConverter.ToArabic("MCMXCIX"));
-            Console.WriteLine(NumberConverter.ToArabic("MCMXCVii"));
-            Console.WriteLine(NumberConverter.ToArabic("mmCcC"));
-            //Console.WriteLine(TNumber.RomanDigitMap["A"]);
-            Console.WriteLine(NumberConverter.ToRoman(2300));
-            Console.WriteLine(NumberConverter.ToRoman(465));
-            Console.WriteLine(NumberConverter.ToRoman(15));
+            ////TNumber.CheckRoman("Hello, World!");
+
+            Console.WriteLine("\n  Arabic to Roman Test\n------------------------\n");
+            foreach (var item in ConvertCases)
+            {
+                switch (item.CheckResult)
+                {
+                    case NumberConverter.CheckResult.OK:
+                        Assert.AreEqual(item.Roman.ToUpper(), NumberConverter.ToRoman(item.Arabic));
+                        Console.WriteLine("  {0,14}  to  {1,14}\n", item.Arabic, item.Roman.ToUpper());
+                        break;
+                    case NumberConverter.CheckResult.Error:
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+            Console.WriteLine("\n  Roman to Arabic Test\n------------------------\n");
+            foreach (var item in ConvertCases)
+            {
+                switch (item.CheckResult)
+                {
+                    case NumberConverter.CheckResult.OK:
+                        Assert.AreEqual(item.Arabic, NumberConverter.ToArabic(item.Roman));
+                        Console.WriteLine("  {0,14}  to  {1,14}\n", item.Roman.ToUpper(), item.Arabic);
+                        break;
+                    case NumberConverter.CheckResult.Error:
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+            Console.WriteLine("\n  Test for input value (Arabic)\n---------------------------------\n");
+            foreach (var item in new int[] { -1, 0, 1, 2, 2998, 2999, 3000, 3001 })
+            {
+                string isCorrect = NumberConverter.CheckArabic(item) == NumberConverter.CheckResult.OK ? "correct" : "incorrect";
+                Console.WriteLine("  {0,14}  is  {1,14}\n", item, isCorrect);
+            }
+
+            Console.WriteLine("\n  Test for input value (Roman)\n--------------------------------\n");
+            foreach (var item in new string[] { "", "IIii", "MDCXLVI", "IVXLCDM", "Hello, World!", "MIM", "Civic", "CcvII", "ABC" })
+            {
+                string isCorrect = NumberConverter.CheckRoman(item) == NumberConverter.CheckResult.OK ? "correct" : "incorrect";
+                Console.WriteLine("  {0,14}  is  {1,14}\n", item, isCorrect);
+            }
         }
     }
 }
